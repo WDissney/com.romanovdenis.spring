030c87f4695b8802a6df8f4db30d44c960175f16
WDissney <denrom1990@yandex.ru>
2023-01-03 16:50:04 +0300

    add Cat class

---
+++ src/main/java/ru/romanovdenis/spring/App.java
@@ -0,0 +1,15 @@
+package ru.romanovdenis.spring;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean =
+                (HelloWorld) context.getBean("helloworld");
+        System.out.println(bean.getMessage());
+        Cat cat = context.getBean(Cat.class);
+    }
+}
---
+++ src/main/java/ru/romanovdenis/spring/Cat.java
@@ -0,0 +1,16 @@
+package ru.romanovdenis.spring;
+
+
+public class Cat {
+
+    private String colour ;
+
+    public void setColor(String color){
+        colour = color;
+    }
+
+    public String getColour (){
+        return colour;
+    }
+
+}
